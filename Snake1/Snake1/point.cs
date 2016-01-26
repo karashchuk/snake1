30b2cb24138da34e40cffaa05f615ab51fcd0cce
pantol <pantolik@rambler.ru>
2016-01-26 21:10:57 +0300

    Сделали класс поинт

---
+++ Snake1/Snake1/point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class point
+    {
+        public int x;
+        public int y;
+        public char sym;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
