255297f83b56d21f60484a734d78a62112cbff72
pantol <pantolik@rambler.ru>
2016-01-27 17:43:54 +0300

    Конструктор. Инкапсуляция

--- Snake1/Snake1/Program.cs
+++ Snake1/Snake1/Program.cs
@@ -10,29 +10,12 @@ namespace Snake1
     {
         static void Main(string[] args)
         {
-            point p1 = new point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            point p1 = new point(1,3,'*');
             p1.Draw();
 
-            point p2 = new point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            point p2 = new point(4,5,'#');
             p2.Draw();
 
-            //int x1 = 1;
-            //int y1 = 3;
-            //char sym1 = '*';
-
-            //Draw(x1, y1, sym1);
-
-            //int x2 = 4;
-            //int y2 = 6;
-            //char sym2 = '#';
-
-            //Draw(x2, y2, sym2);
             Console.ReadLine();
 
         }
--- Snake1/Snake1/point.cs
+++ Snake1/Snake1/point.cs
@@ -11,6 +11,18 @@ namespace Snake1
         public int x;
         public int y;
         public char sym;
+        public point()
+        {
+           
+        }
+
+        public point(int _x,int _y,char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
