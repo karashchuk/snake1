c8baf8c4a701dc41ffc1d9f6662b56f8c3060040
pantol <pantolik@rambler.ru>
2016-01-28 21:14:03 +0300

    Классы горизонтальных и вертикальных линий

---
+++ Snake1/Snake1/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class VerticalLine
+    {
+        List<point> pList;
+        public VerticalLine (int x, int yUp,int yDown,char sym)
+        {
+            pList = new List<point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                point p = new point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void vDraw()
+        {
+            foreach(point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+    }
+}
---
+++ Snake1/Snake1/horosontalline.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class horosontalline
+    {
+        List<point> pList;
+        public horosontalline(int xLeft,int XRight, int y, Char sym)
+        {
+            pList = new List<point>();
+            for (int x = xLeft; x <= XRight; x++)
+            {
+                point p = new point(x, y, sym);
+                pList.Add(p);
+            }
+
+           
+        }
+
+        public void hDraw()
+        {
+            foreach (point p in pList)
+            {
+                p.Draw();
+            }
+
+        }
+
+
+    }
+}
