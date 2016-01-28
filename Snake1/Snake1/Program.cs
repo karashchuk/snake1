3255324c3c344e9f03f4d1098972f23c315bd83c
pantol <pantolik@rambler.ru>
2016-01-28 19:27:03 +0300

    Class List

--- Snake1/Snake1/Program.cs
+++ Snake1/Snake1/Program.cs
@@ -16,6 +16,35 @@ namespace Snake1
             point p2 = new point(4,5,'#');
             p2.Draw();
 
+            List<int> Numlist = new List<int>();
+            Numlist.Add(0);
+            Numlist.Add(1);
+            Numlist.Add(2);
+
+            int x = Numlist[0];
+            int y = Numlist[1];
+            int z = Numlist[2];
+
+            foreach(int i in Numlist)
+            {
+                Console.WriteLine(i);
+            }
+
+            Numlist.RemoveAt(0);
+
+            List<point> pList= new List<point>();
+
+            pList.Add(p1);
+            pList.Add(p2);
+
+            List<Char> CharList = new List<Char>();
+            CharList.Add('*');
+            CharList.Add('#');
+            CharList.Add('$');
+            CharList.Add('&');
+
+
+
             Console.ReadLine();
 
         }
