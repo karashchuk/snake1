98cfeacf39ee3cec88c8aaa8ed78122960868fd3
pantol <pantolik@rambler.ru>
2016-01-29 21:18:16 +0300

    Дополнительный класс для наследования в классах горизонтальных и вертикальных линий

---
+++ Snake1/Snake1/Figure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake1
+{
+    class Figure
+    {
+        protected List<point> pList;
+
+        public void Drow()
+        {
+            foreach (point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+
+    }
+}
