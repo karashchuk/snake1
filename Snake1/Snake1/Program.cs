1ebd697502802df3dcfe94bc85a2651dfa2f68e9
pantol <pantolik@rambler.ru>
2016-01-29 22:07:43 +0300

    Создал класс Змейка

--- Snake1/Snake1/Program.cs
+++ Snake1/Snake1/Program.cs
@@ -24,7 +24,8 @@ namespace Snake1
 
             //Отрисовка точек
             point p = new point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
             Console.ReadLine();
 
